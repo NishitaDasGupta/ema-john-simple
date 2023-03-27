0f46d85748bb46b21f927c23ff66910f20affb82
NishitaDasGupta <nishitadasguptamou@gmail.com>
2023-03-27 14:01:39 +0600

    product-img

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,20 +1,25 @@
 import React, { useEffect, useState } from 'react';
+import Product from '../Product/Product';
 import './Shop.css'
 const Shop = () => {
-    const [products , setProducts] = useState([]);
-    useEffect(()=>{
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
         fetch('../../../public/products.json')
-        .then(res => res.json())
-        .then(data => setProducts(data))
-    },[])
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, [])
     return (
         <div className='shop-container'>
             <div className="product-container">
+                {products.map(product => console.log(product))}
+                {products.map(product => <Product 
+                key={product.id}
+                product={product}
+                ></Product>)}
 
-<h1>{products.length}</h1>
             </div>
             <div className="cart-container">
-<h1>Bye</h1>
+                <h1>Bye</h1>
             </div>
         </div>
     );
