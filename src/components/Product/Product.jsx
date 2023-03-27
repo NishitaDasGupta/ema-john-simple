7bc2e3b2ffc63f4c586c93ec570bd74154a80b28
NishitaDasGupta <nishitadasguptamou@gmail.com>
2023-03-27 19:54:32 +0600

    cart

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import './Product.css'
-
+import cartPlus from '../../images/cart-plus-solid.svg'
 const Product = (props) => {
     const {img,name,price,seller,ratings} = props.product;
     return (
         <div className='product'>
-            <img src={img} alt="" />
+            <img className='product-img' src={img} alt="" />
+            <div className='product-info'>
             <h6>{name}</h6>
+            <p className='price'>Price: ${price}</p>
+            <p>Manufacturer: {seller}</p>
+            <p>Rating: {ratings} Stars</p>
+            </div>
+            <button>Add to Cart <img className='btn-pic' src={cartPlus} alt="" /></button>
         </div>
     );
 };
